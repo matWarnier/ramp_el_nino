7886d1af816209d716116029b906c38ab3ed865e
matthieu <matthieu@plumelabs.com>
2015-06-16 15:44:56 +0200

    remove PCA and add max depth

--- regressor.py
+++ regressor.py
@@ -7,8 +7,8 @@ from sklearn.decomposition import KernelPCA
 class Regressor(BaseEstimator):
     def __init__(self):
         self.clf = Pipeline([('scaler', StandardScaler()),
-                             ('KernelPCA',KernelPCA(n_components=10)),
-                             ("RF", RandomForestRegressor(n_estimators=100))])
+                             # ('KernelPCA',KernelPCA(n_components=10)),
+                             ("RF", RandomForestRegressor(n_estimators=100, max_depth=15))])
 
     def fit(self, X, y):
         self.clf.fit(X, y)
